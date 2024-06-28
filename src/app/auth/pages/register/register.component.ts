c573273c3db154769fdb1dd7471fa919b1b39292
Nilton Esdiver Caro <niltonescato123@gmail.com>
2024-06-27 21:47:01 -0500

    validators en html von *ngIf

--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import  * as customValidators from 'src/app/shared/validators/validators';
+//import  * as customValidators from 'src/app/shared/validators/validators';
 import { ValidatorsService } from '../../../shared/services/validators.service';
 
 @Component({
@@ -12,9 +12,9 @@ export class RegisterComponent {
 
 
   public myForm: FormGroup= this.fb.group({
-    name:['',[Validators.required,Validators.pattern(customValidators.firstNameAndLastnamePattern)]],
-    email:['',[Validators.required,Validators.pattern(customValidators.emailPattern)]],
-    username:['',[Validators.required, customValidators.cantBeStrider]],
+    name:['',[Validators.required,Validators.pattern(this.validatorsServices.firstNameAndLastnamePattern)]],
+    email:['',[Validators.required,Validators.pattern(this.validatorsServices.emailPattern)]],
+    username:['',[Validators.required, this.validatorsServices.cantBeStrider]],
     password:['',[Validators.required,Validators.minLength(6)]],
     password2:['',[Validators.required]],
   });
@@ -26,6 +26,7 @@ export class RegisterComponent {
 
   isValidField(field:string){
     //deve optener validacion desde un servicio
+    return this.validatorsServices.isValidField(this.myForm,field)
   }
 
   onSubmit(){
